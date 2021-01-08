0eda6d5f20ce2402c4af6365d5c2c999d55a0f8a
hamatatsu <tatsuyahamada.g@gmail.com>
2021-01-08 17:56:41 +0900

    wincheck

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-# %% import
+# %% ライブラリインポート
 import matplotlib.pyplot as plt
 import numpy as np
 
-population_num = 100
-chromosome_len = pow(3, 9)  # 3^9
-value_max = 9
+# %% グローバル変数
+POPULATION_NUM = 100
+CHROMOSOME_LEN = pow(3, 9)
+VALUE_MAX = 9
 
-THREE = np.power(3, np.arange(9))  # 公比3の等比数列
-winline = np.array([
+# 公比3の等比数列
+THREE = np.power(3, np.arange(9))
+# 並び判定用配列
+WINLANE = np.array([
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
@@ -17,7 +20,7 @@ winline = np.array([
 # %% generate
 def generate():
   # generate random int 1~9
-  population = np.random.randint(1, value_max+1, (population_num, chromosome_len))
+  population = np.random.randint(1, VALUE_MAX+1, (POPULATION_NUM, CHROMOSOME_LEN))
   return population
 
 
@@ -27,22 +30,32 @@ def makeindex(board):
   return makeindex
 
 
+# %% 並び判定
+def wincheck(board, turnflag):
+  win = np.any(np.all(board[WINLANE] == turnflag, axis=1))
+  return win
+
+
 # %% まるばつゲーム
 def marubatsu(batsu, maru):
-  result = 0  # 勝敗 先攻 1, 後攻 -1, 引き分け 0
+  # 勝敗 先攻 1, 後攻 -1, 引き分け 0
+  result = 0
   board = np.zeros(9)
   # 勝敗が付くまでループ
   for turn in range(9):
-    turnflag = turn % 2 + 1  # 手番 先攻 1, 後攻 2
+    # 手番 先攻 1, 後攻 2
+    turnflag = turn % 2 + 1
     index = makeindex(board)
     # 既に印がある時
     if board[index] != 0:
-      result = turnflag * 2 - 3  # 手番の逆が勝利
+      # 手番の逆が勝利
+      result = turnflag * 2 - 3
       break
     board[index] = turnflag
     # 3つ並んだ時
-    if wincheck(board, 1):
-      result = 3 - turnflag * 2  # 手番が勝利
+    if wincheck(board, turnflag):
+      # 手番が勝利
+      result = 3 - turnflag * 2
       break
   return result
 
